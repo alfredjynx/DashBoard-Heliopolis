5b82c22fd755fc9c6e3a2c2f2bdf2d9cd831c300
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2023-06-06 22:51:28 -0300

    sono

--- init.py
+++ init.py
@@ -3,12 +3,20 @@ import pandas as pd
 import plotly.express as px
 import requests
 
-# Getting denuncias from localhost and transforming to dataframe
-get_denuncias = requests.get("https://3618-186-232-61-4.ngrok-free.app/denuncia")
-#---------------------------------------------------------------
-app = Dash(__name__)
+get_denuncias = requests.get("http://3.23.96.142:8080/denuncia")
 
 df = pd.DataFrame(get_denuncias.json())
+df = df.drop(columns=['link','mediaType'])
+
+getMonth = lambda d: d.month
+
+df['dataEnchente'] = pd.to_datetime((df['dataEnchente']),format='%Y-%m-%d')
+df['dataDenuncia'] = pd.to_datetime((df['dataDenuncia']),format='%Y-%m-%d')
+
+df['mesEnchente'] = df['dataEnchente'].apply(getMonth)
+df['mesDenuncia'] = df['dataDenuncia'].apply(getMonth)
+
+app = Dash(__name__)
 
 app.layout = html.Div([
     html.Div([
@@ -17,7 +25,11 @@ app.layout = html.Div([
         ], style={"padding": 100, "color": "white", "background-color": "black"}),
     dcc.Graph(figure=px.histogram(df, x='dataDenuncia', title="Denúncias por Data").update_layout(xaxis_title="Data", yaxis_title="Número de Denúncias"), style={"padding": "0.1%", "background-color": "#9a9c9a"}),
     dcc.Graph(figure=px.histogram(df, x='local', title="Denúncias por Local").update_layout(xaxis_title="local", yaxis_title="Número de Denúncias"), style={"padding": "0.1%", "background-color": "#9a9c9a"}),
-    dcc.Graph(figure=px.pie(df, names='local', title='Population of European continent'))
+    dcc.Graph(figure=px.histogram(df, x='mesDenuncia', title="Denúncias por Mes").update_layout(xaxis_title="Mês do Ano (número)", yaxis_title="Número de Denúncias"), style={"padding": "0.1%", "background-color": "#9a9c9a"}),
+    dcc.Graph(figure=px.histogram(df, x='mesEnchente', title="Enchentes por Mes").update_layout(xaxis_title="Mês do Ano (número)", yaxis_title="Número de Enchentes Relatadas"), style={"padding": "0.1%", "background-color": "#9a9c9a"}),
+    dcc.Graph(figure=px.pie(df, names='local', title='Porcentagem de Denúncias por Região'),style={"padding": "0.1%", "background-color": "#9a9c9a"}),
+    dcc.Graph(figure=px.pie(df, names='mesDenuncia', title='Porcentagem de Denúncias por Mes'),style={"padding": "0.1%", "background-color": "#9a9c9a"}),
+    dcc.Graph(figure=px.pie(df, names='mesEnchente', title='Porcentagem de Enchente Relatadas por Mes'),style={"padding": "0.1%", "background-color": "#9a9c9a"})
 ], style={'padding':0, "margin":0})
 
 if __name__ == '__main__':
