f2d32425087097ab4d156f35f8f9f1b97d2c7b1b
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2023-06-07 15:37:09 -0300

    trocagem dos meses para string

--- init.py
+++ init.py
@@ -8,10 +8,16 @@ get_denuncias = requests.get("http://3.23.96.142:8080/denuncia")
 df = pd.DataFrame(get_denuncias.json())
 df = df.drop(columns=['link','mediaType'])
 
-getMonth = lambda d: d.month
+Mêses = ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
+
+getMonth = lambda d: Mêses[d.month-1]
+getDate = lambda d: d.date()
 
 df['dataEnchente'] = pd.to_datetime((df['dataEnchente']),format='%Y-%m-%d')
 df['dataDenuncia'] = pd.to_datetime((df['dataDenuncia']),format='%Y-%m-%d')
+
+df['dataEnchente'] = df['dataEnchente'].apply(getDate)
+df['dataDenuncia'] = df['dataDenuncia'].apply(getDate)
 
 df['mesEnchente'] = df['dataEnchente'].apply(getMonth)
 df['mesDenuncia'] = df['dataDenuncia'].apply(getMonth)
@@ -36,11 +42,11 @@ app.layout = html.Div([
     ],style={'padding':0, 'margin':0,'display':'flex','flex-direction':'row','height':'10%'}),
     dcc.Graph(figure=px.histogram(df, x='dataDenuncia', title="Denúncias por Data").update_layout(xaxis_title="Data", yaxis_title="Número de Denúncias"), style={"padding-bottom": "0.1%", "background-color": "#9a9c9a"}),
     dcc.Graph(figure=px.histogram(df, x='local', title="Denúncias por Região").update_layout(xaxis_title="Região", yaxis_title="Número de Denúncias"), style={"padding-bottom": "0.1%", "background-color": "#9a9c9a"}),
-    dcc.Graph(figure=px.histogram(df, x='mesDenuncia', title="Denúncias por Mes").update_layout(xaxis_title="Mês do Ano (número)", yaxis_title="Número de Denúncias"), style={"padding-bottom": "0.1%", "background-color": "#9a9c9a"}),
-    dcc.Graph(figure=px.histogram(df, x='mesEnchente', title="Enchentes por Mes").update_layout(xaxis_title="Mês do Ano (número)", yaxis_title="Número de Enchentes Relatadas"), style={"padding-bottom": "0.1%", "background-color": "#9a9c9a"}),
+    dcc.Graph(figure=px.histogram(df, x='mesDenuncia', title="Denúncias por Mês").update_layout(xaxis_title="Mês do Ano", yaxis_title="Número de Denúncias"), style={"padding-bottom": "0.1%", "background-color": "#9a9c9a"}),
+    dcc.Graph(figure=px.histogram(df, x='mesEnchente', title="Enchentes por Mês").update_layout(xaxis_title="Mês do Ano", yaxis_title="Número de Enchentes Relatadas"), style={"padding-bottom": "0.1%", "background-color": "#9a9c9a"}),
     dcc.Graph(figure=px.pie(df, names='local', title='Porcentagem de Denúncias por Região'),style={"padding-bottom": "0.1%", "background-color": "#9a9c9a"}),
-    dcc.Graph(figure=px.pie(df, names='mesDenuncia', title='Porcentagem de Denúncias por Mes'),style={"padding-bottom": "0.1%", "background-color": "#9a9c9a"}),
-    dcc.Graph(figure=px.pie(df, names='mesEnchente', title='Porcentagem de Enchente Relatadas por Mes'),style={"padding-bottom": "0.1%", "background-color": "#9a9c9a"})
+    dcc.Graph(figure=px.pie(df, names='mesDenuncia', title='Porcentagem de Denúncias por Mês'),style={"padding-bottom": "0.1%", "background-color": "#9a9c9a"}),
+    dcc.Graph(figure=px.pie(df, names='mesEnchente', title='Porcentagem de Enchente Relatadas por Mês'),style={"padding-bottom": "0.1%", "background-color": "#9a9c9a"})
 ], style={'padding':0, "margin":0,"font-family": "Open Sans,sans-serif"})
 
 if __name__ == '__main__':
